7eb947c7da57fe32edc11c94e193c33d6c313741
cosmo-18 <cosimo.simonesolfrizzi@gmail.com>
2023-01-25 17:38:15 +0100

    all-code

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index () {
+
+        $Movies = Movie::all();
+
+        return view ('welcome', compact('Movies'));
+    }
+}
